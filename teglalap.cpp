f8d08a61938a45fdba30688f96410e357ef40c54
TucsokSk1lll <jgyorgy008@gmail.com>
2022-12-01 20:45:14 +0100

    anyadat

---
+++ teglalap.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+    
+
+    int x = 10;
+    int y = 5;
+
+    for (size_t i = 0; i < x; i++)
+    {
+        cout << "x";
+    }
+    cout << "\n";
+    for (size_t i = 0; i < y-2; i++)
+    {
+        cout << "x";
+        for (size_t j = 0; j < x-2; j++)
+        {
+            cout << " ";
+        }
+        cout << "x\n";
+    }
+    for (size_t i = 0; i < x; i++)
+    {
+        cout << "x";
+    }
+    cout << "\n";
+    
+}
